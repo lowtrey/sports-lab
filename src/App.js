396cd20678117a36d72091e935fc238f81492aa2
Q. Price <priceqp@gmail.com>
2020-02-05 00:35:04 -0600

    Complete Team component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Team from "./Team";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Team name="Lakers" logo="/lakers.png" />
+      <Team name="Warriors" logo="/warriors.png" />
     </div>
   );
 }
