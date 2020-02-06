74a135a9952a626c04b2bdc5af679de8d0b4345f
Q. Price <priceqp@gmail.com>
2020-02-05 21:35:29 -0600

    Complete exercise

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 import Team from "./Team";
 import "./Game.css";
 
-const Game = props => {
-  return (
-    <>
-      <h1>Welcome To {props.venue}!</h1>
-      <div className="Game">
-        <Team name="Lakers" logo="/lakers.png" />
-        <Team name="Warriors" logo="/warriors.png" />
-      </div>
-    </>
-  );
-};
+const shoot = new Audio("/shoot.wav");
+const swish = new Audio("/swish.wav");
+const initialTeams = [
+  {
+    logo: "/lakers.png",
+    name: "Lakers",
+    shotsTaken: 0,
+    side: "Home",
+    score: 0
+  },
+  {
+    logo: "/warriors.png",
+    name: "Warriors",
+    side: "Visiting",
+    shotsTaken: 0,
+    score: 0
+  }
+];
+
+class Game extends Component {
+  state = {
+    teams: initialTeams
+  };
+
+  handleShoot = event => {
+    const currentSide = event.target.id;
+    const pointsScored = Math.random() >= 0.5 ? 1 : 0;
+    const sound = pointsScored === 1 ? swish : shoot;
+    sound.currentTime = 0;
+    sound.play();
+    // Make Copy
+    const newTeams = this.state.teams.slice();
+    // Modify Copy
+    newTeams.map(team => {
+      if (team.side === currentSide) {
+        team.shotsTaken += 1;
+        team.score += pointsScored;
+      }
+      return team;
+    });
+    //Overwrite Original With The Copy
+    this.setState({ teams: newTeams });
+  };
+
+  render() {
+    const [home, visiting] = this.state.teams;
+    return (
+      <>
+        <h1>Welcome To {this.props.venue}!</h1>
+        <div className="Game">
+          <Team {...home} handleShoot={this.handleShoot} />
+          <Team {...visiting} handleShoot={this.handleShoot} />
+        </div>
+      </>
+    );
+  }
+}
 
 export default Game;
--- src/Team.js
+++ src/Team.js
@@ -1,41 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Team.css";
 
-const shoot = new Audio("/shoot.wav");
-const swish = new Audio("/swish.wav");
-
-class Team extends Component {
-  state = {
-    shotsTaken: 0,
-    score: 0
-  };
-
-  letItFly = () => {
-    const pointsScored = Math.random() >= 0.5 ? 1 : 0;
-    const sound = pointsScored === 1 ? swish : shoot;
-    sound.currentTime = 0;
-    sound.play();
-    this.setState(prevState => ({
-      shotsTaken: prevState.shotsTaken + 1,
-      score: prevState.score + pointsScored
-    }));
-  };
-
-  render() {
-    const { name, logo } = this.props;
-    const { shotsTaken, score } = this.state;
-    const percentage = Number(score / shotsTaken).toFixed(2);
-    return (
-      <div className="Team">
-        <h1>{name}</h1>
-        <img src={logo} alt="Team Logo" />
-        <h2>Shots Taken: {shotsTaken}</h2>
-        <h2>Score: {score}</h2>
-        {shotsTaken > 0 && <h2>Shooting % : {percentage}</h2>}
-        <button onClick={this.letItFly}>SHOOT!</button>
-      </div>
-    );
-  }
-}
+const Team = props => {
+  const percentage = Number(props.score / props.shotsTaken).toFixed(2);
+  const { name, logo, shotsTaken, score, side, handleShoot } = props;
+  return (
+    <div className="Team">
+      <h1>{name}</h1>
+      <img src={logo} alt="Team Logo" />
+      <h2>Shots Taken: {shotsTaken}</h2>
+      <h2>Score: {score}</h2>
+      {shotsTaken > 0 && <h2>Shooting % : {percentage}</h2>}
+      <button id={side} onClick={handleShoot}>
+        LET IT FLYYY!
+      </button>
+    </div>
+  );
+};
 
 export default Team;
